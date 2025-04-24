997b6e6509b1cd259326d57d401aca56b6827cd6
Brad Carman <bradleygcarman@outlook.com>
2025-04-24 16:08:35 +0100

    update

--- bin/main.jl
+++ bin/main.jl
@@ -2,14 +2,74 @@ using HTTP, Sockets, UUIDs, JSON2
 
 # modified Animal struct to associate with specific user
 mutable struct Animal
-    id::Int
-    userId::UUID
     type::String
     name::String
 end
 
+struct TimeSeriesEvent
+    timeStamp::String
+    value::Number
+end
+
+#=
+
+interface TimeSeriesEvent {
+  timeStamp: string;
+  value: number;
+}
+
+=#
+
+struct TagResponse
+    tagName::String
+    tagID::Int
+    appContextGuid::String
+    events::Vector{TimeSeriesEvent}
+end
+
+#=
+
+interface TagResponse {
+  tagName: string;
+  tagId: number;
+  appContextGuid: string;
+  events: TimeSeriesEvent[];
+}
+
+interface RequestBody {
+  tags: Array<{ tagName: string; tagId: number }>;
+  startDate: string;
+  endDate: string;
+  appContextGuid: string;
+}
+
+=#
+
+struct TagItem
+    tagName::String
+    tagId::Number
+end
+struct RequestBody
+    tags::Vector{TagItem}
+    startDate::String
+    endDate::String
+    appContextGuid::String
+end
+
+function helixRequest(req::HTTP.Request)
+    req_body = JSON2.read(IOBuffer(HTTP.payload(req)), RequestBody)
+    # run model and return data...
+
+    response = [TagResponse(tag.tagName, tag.tagId, req_body.appContextGuid, [TimeSeriesEvent("4/24/2025 - 4:05PM", 0.0)]) for tag in tags]
+  
+    return HTTP.Response(200, JSON2.write(response))
+end
+
 # use a plain `Dict` as a "data store"
 const ANIMALS = Dict{Int, Animal}()
+ANIMALS[1] = Animal("Dog","Brad")
+ANIMALS[2] = Animal("Cat","Siva")
+ANIMALS[2] = Animal("Fish","Marcus")
 const NEXT_ID = Ref(0)
 function getNextId()
     id = NEXT_ID[]
@@ -49,11 +109,13 @@ end
 
 # define REST endpoints to dispatch to "service" functions
 const ANIMAL_ROUTER = HTTP.Router()
-HTTP.@register(ANIMAL_ROUTER, "POST", "/api/zoo/v1/animals", createAnimal)
+HTTP.register!(ANIMAL_ROUTER, "POST", "/api/zoo/v1/animals", createAnimal)
 # note the use of `*` to capture the path segment "variable" animal id
-HTTP.@register(ANIMAL_ROUTER, "GET", "/api/zoo/v1/animals/*", getAnimal)
-HTTP.@register(ANIMAL_ROUTER, "PUT", "/api/zoo/v1/animals", updateAnimal)
-HTTP.@register(ANIMAL_ROUTER, "DELETE", "/api/zoo/v1/animals/*", deleteAnimal)
+HTTP.register!(ANIMAL_ROUTER, "GET", "/api/zoo/v1/animals/*", getAnimal)
+HTTP.register!(ANIMAL_ROUTER, "PUT", "/api/zoo/v1/animals", updateAnimal)
+HTTP.register!(ANIMAL_ROUTER, "DELETE", "/api/zoo/v1/animals/*", deleteAnimal)
+
+HTTP.register!(ANIMAL_ROUTER, "GET", "api/helix", helixRequest)
 
 # Start the server
 HTTP.serve(ANIMAL_ROUTER, ip"0.0.0.0", parse(Int, get(ENV, "PORT", "8080")))
